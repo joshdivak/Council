ec7b5e8ad90d682938b98d50344c97982ab1c239
Josh Divak <joshdivak2023@u.northwestern.edu>
2020-12-10 16:39:37 -0500

    Added second deck and generate cards on click

--- main.js
+++ main.js
@@ -1,41 +1,76 @@
-//Create a deck with 20 cards and two players  
-let cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// //Create a deck with 20 cards and two players  
+// let cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+// let player1 = [];
+// let player2 = [];
+
+let actcards = ['ACT1','ACT2','ACT3','ACT4','ACT5','ACT6'];
+let supportcards = ['SUPPORT1','SUPPORT2','SUPPORT3','SUPPORT4','SUPPORT5','SUPPORT6'];
+let cardsdealt = [];
 let player1 = [];
 let player2 = [];
 
-//assign 10 random cards to player 1
-for (i=0; i<10; i++) {
-    rand = Math.floor(Math.random()*19);
-    while (player1.includes(cards[rand])) {
-        rand = Math.floor(Math.random()*19);
+// //deal act or support card
+// var deal = 1;
+// //generates random act card and updates your hand
+// function dealAct() {
+//     card = document.getElementById('card'+deal);
+//     console.log(card);
+//     rand = Math.floor(Math.random()*6);
+//     //ensures no duplicates
+//     while (cardsplayed.includes(actcards[rand])) {
+//         rand = Math.floor(Math.random()*6);
+//     }
+//     cardsplayed.push(actcards[rand]);
+//     card.innerHTML = cardsplayed[deal-1];
+//     deal++;
+// }
+// //generates random support card and updates your hand
+// function dealSupport() {
+//     card = document.getElementById('card'+deal);
+//     console.log(card);
+//     rand = Math.floor(Math.random()*6);
+//     //ensures no duplicates
+//     while (cardsplayed.includes(supportcards[rand])) {
+//         rand = Math.floor(Math.random()*6);
+//     }
+//     cardsplayed.push(supportcards[rand]);
+//     card.innerHTML = cardsplayed[deal-1];
+//     deal++;
+// }
+
+// //plays the card you click 
+// function playCard(clickedElement) {
+//     let destination = document.getElementById('inplay'); //sets destination to inplay container
+//     destination.appendChild(clickedElement); //adds clicked card to inplay
+// }
+
+function randomCard(cardtype) {
+    rand = Math.floor(Math.random()*6); //replace 6 with actual number of cards per deck
+    //ensures no duplicates
+    while (cardsdealt.includes(cardtype[rand])) {
+        rand = Math.floor(Math.random()*6);
     }
-    player1.push(cards[rand]);
+    cardsdealt.push(cardtype[rand]);
+    return cardtype[rand];
 }
 
-//assign remaining 10 cards to player 2
-for(i=0; i<cards.length; i++) {
-    if (!player1.includes(cards[i])) {
-        player2.push(cards[i]);
+function drawCard(cardtype) {
+    //creates div called 'card' of class 'placeholder' with onclick function
+    const card = document.createElement('div');
+    card.classList.add('card')
+    card.addEventListener('click', function (event) {
+        let destination = document.getElementById('inplay'); //sets destination to inplay container
+        destination.appendChild(card); //adds clicked card to inplay
+    });
+    const para = document.createElement("P");
+    if (cardtype === "act") {
+        var text = randomCard(actcards);
     }
+    else {
+        var text = randomCard(supportcards);
+    }
+    para.innerHTML = text;
+    card.appendChild(para);
+    hand.appendChild(card);
 }
 
-//check that ^^ works correctly
-console.log(player1)
-console.log(player2)
-
-//deal cards to hand when you click the deck
-deal=1
-function dealCard() {
-    // document.querySelector('#card1').innerHTML = player1[0];
-    //create element called card1, then card2, etc ??
-    card = document.getElementById('card'+deal);
-    //prints the number on the card
-    card.innerHTML = player1[deal-1];
-    deal++
-}
-
-//plays the card you click 
-function playCard(clickedElement) {
-    let destination = document.getElementById('inplay'); //sets destination to inplay container
-    destination.appendChild(clickedElement); //adds clicked card to inplay
-}
