a23f5b42eddaf4cdfe831c92d733245a4f41b4e6
Megan Lagerquist <mlagerq@emory.edu>
2020-12-20 21:56:17 -0500

    server.js and scroll in box

---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host:'flexdecks.gametrainlearning.org',
+    database:'gametrai_flexdecks',
+    user: 'gametrai_megan',
+    password:'play2learn',
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(error, results, fields){
+    if (error) throw error;
+    console.log('The solution is: ', results[0].solution);
+});
+
+connection.end();
